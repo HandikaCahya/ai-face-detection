d0f6a3a6f52a841113b815b9dd7e9fe87cea3db3
HandikaCahya <handikacw@gmail.com>
2026-06-04 06:53:38 +0700

    ai face detection deploy

--- main.py
+++ main.py
@@ -296,4 +296,4 @@ async def predict(file: UploadFile = File(...)):
 
 
 if __name__ == "__main__":
-    uvicorn.run("main:app", host="0.0.0.0", port=8001, reload=True)
+    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
